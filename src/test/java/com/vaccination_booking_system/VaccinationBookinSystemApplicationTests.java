66df5c7c30fe5898b0467a0c1c486ad82f1ea552
MushfiqueAlam <mushfique.037@gmail.com>
2025-02-13 17:42:54 +0530

    create vaccination booking system and connet to databases

---
+++ src/test/java/com/vaccination_booking_system/VaccinationBookinSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccination_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationBookinSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
